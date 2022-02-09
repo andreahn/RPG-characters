2f8f85b801248fc6abf04f1152d14ea7f08c38e6
ANakst <andrea.nakstad@yahoo.com>
2022-02-09 12:20:54 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RPG_characters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# RPG Character Console Program
+
+
+## Installation
+
+
+## Usage
+
+
+## Contributing
+Andrea Hårseth Nakstad [@andreahn](https://github.com/andreahn)
