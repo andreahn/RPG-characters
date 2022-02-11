5ebbb1dfbdc95a832f7f13d32ab33ccbbda22a15
Andrea Nakstad <andrea.nakstad@yahoo.com>
2022-02-11 14:54:41 +0100

    feat: added enumerators for slot, weapon and armor

--- Program.cs
+++ Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace RPG_characters
 {
-    internal class Program
+    enum Slot
     {
+        SLOT_HEAD,
+        SLOT_BODY,
+        SLOT_LEGS,
+        SLOT_WEAPON
+    }
+
+    enum WeaponType
+    {
+        WEAPON_AXE,
+        WEAPON_BOW,
+        WEAPON_DAGGER,
+        WEAPON_HAMMER,
+        WEAPON_STAFF,
+        WEAPON_SWORD,
+        WEAPON_WAND
+    }
+
+    enum ArmourType
+    {
+        ARMOUR_CLOTH,
+        ARMOUR_LEATHER,
+        ARMOUR_MAIL,
+        ARMOUR_PLATE
+    }
+
+    class Program
+    {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
