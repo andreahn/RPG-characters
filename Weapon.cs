4f4eeb144a31a0d6174bb7946da77ff4ff2a88e4
Andrea Nakstad <andrea.nakstad@yahoo.com>
2022-02-11 14:47:24 +0100

    feat: created weapon class

---
+++ Weapon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RPG_characters
+{
+    class Weapon : Item
+    {
+
+        #region Variables
+        private WeaponType itemType;
+        //todo: armour attributes
+        #endregion
+
+        #region Properties
+        public WeaponType ItemType { get => itemType; }
+        // todo: armour attributes
+        #endregion
+
+        #region Constructors
+        //constructor here
+        #endregion
+
+        #region Methods
+        //methods here
+        #endregion
+    }
+}
