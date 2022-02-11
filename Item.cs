060508bbcbd99cb3ac723978c8356f6d1e2562c4
Andrea Nakstad <andrea.nakstad@yahoo.com>
2022-02-11 14:39:50 +0100

    feat: created item class

---
+++ Item.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace RPG_characters
+{
+    abstract class Item
+    {
+        #region Variables
+        protected string itemName;
+        protected int itemLevel;
+        protected Slot itemSlot1;
+        #endregion
+
+        #region Properties
+        public string ItemName { get { return itemName; } }
+        public int ItemLevel { get { return itemLevel; } }
+        public Slot ItemSlot { get => itemSlot1; set => itemSlot1 = value; }
+        #endregion
+
+        #region Constructors
+        //constructor here
+        #endregion
+
+        #region Methods
+        //methods here
+        #endregion
+    }
+}
