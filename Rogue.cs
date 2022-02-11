0283b6b7ae9ba739944a225802df76524cafd21b
Andrea Nakstad <andrea.nakstad@yahoo.com>
2022-02-11 14:28:37 +0100

    feat: created rogue class

---
+++ Rogue.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_characters
+{
+    class Rogue : Character
+    {
+        public Rogue()
+        {
+            compatibleArmour = new ArmourType[] { ArmourType.ARMOUR_LEATHER, ArmourType.ARMOUR_MAIL };
+            compatibleWeapons = new WeaponType[] { WeaponType.WEAPON_DAGGER, WeaponType.WEAPON_SWORD };
+            levelUpAttributeValues = new PrimaryAttributes() { Dexterity = 4, Intelligence = 1, Strength =  1};
+            attributes = new PrimaryAttributes() { Dexterity = 6, Intelligence = 1, Strength = 2 };
+        }
+        //public override void characterType()
+        //{
+        //    Console.WriteLine("rogue");
+        //}
+    }
+}
