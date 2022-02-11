0eff02ba9977fe8418a193a39076de7277a9c849
Andrea Nakstad <andrea.nakstad@yahoo.com>
2022-02-11 14:46:08 +0100

    feat: created armour class

---
+++ Armour.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RPG_characters
+{
+    class Armour : Item
+    {
+
+        #region Variables
+        private ArmourType itemType;
+        private PrimaryAttributes armourAttributes;
+        #endregion
+
+        #region Properties
+        public ArmourType ItemType { get => itemType; }
+        public PrimaryAttributes ArmourAttributes { get => armourAttributes; }
+        #endregion
+
+        #region Constructors
+        //constructor here
+        #endregion
+
+        #region Methods
+        //methods here
+        #endregion
+    }
+}
