93cc15a2b6c1b78e722dec6c18ee08be9b7feba1
Andrea Nakstad <andrea.nakstad@yahoo.com>
2022-02-11 14:28:02 +0100

    feat: created mage class

---
+++ Mage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RPG_characters
+{
+    class Mage : Character
+    {
+        public Mage()
+        {
+            compatibleArmour = new ArmourType[] { ArmourType.ARMOUR_CLOTH };
+            compatibleWeapons = new WeaponType[] { WeaponType.WEAPON_STAFF, WeaponType.WEAPON_WAND };
+            levelUpAttributeValues = new PrimaryAttributes() { Dexterity = 1, Intelligence = 5, Strength =  1};
+            attributes = new PrimaryAttributes() { Dexterity = 1, Intelligence = 8, Strength = 1 };
+            //equipment.Add(Slot.SLOT_HEAD, new Armour() { ItemName = "Some item", ItemType = ArmourType.ARMOUR_CLOTH });
+        }
+        //public override void characterType()
+        //{
+        //    Console.WriteLine("mage");
+        //}
+
+    }
+}
