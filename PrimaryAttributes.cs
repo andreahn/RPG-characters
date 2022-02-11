86df2027a63eab88fea9368c27b4b3c6eb372a87
Andrea Nakstad <andrea.nakstad@yahoo.com>
2022-02-11 14:29:20 +0100

    feat: created primary attribute class

---
+++ PrimaryAttributes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_characters
+{
+    class PrimaryAttributes
+    {
+        public int Strength { get; set; }
+        public int Dexterity { get; set; }
+        public int Intelligence { get; set; }
+
+        public PrimaryAttributes()
+        {
+            Strength = 0;
+            Dexterity = 0;
+            Intelligence = 0;
+        }
+
+        public static PrimaryAttributes operator+ (PrimaryAttributes Attributes, PrimaryAttributes AttributeUpdateValues)
+        {
+            Attributes.Strength += AttributeUpdateValues.Strength;
+            Attributes.Dexterity += AttributeUpdateValues.Dexterity;
+            Attributes.Intelligence += AttributeUpdateValues.Intelligence;
+            return Attributes;
+        }
+
+
+    }
+}
