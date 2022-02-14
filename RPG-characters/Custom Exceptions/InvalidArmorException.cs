0e0d11b7337c0494377b971ddb588faa22a971e4
Andrea Nakstad <andrea.nakstad@yahoo.com>
2022-02-14 15:37:53 +0100

    feat: created custom exceptions

---
+++ RPG-characters/Custom Exceptions/InvalidArmorException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_characters.Custom_Exceptions
+{
+    public class InvalidArmorException : Exception
+    {
+        public InvalidArmorException(string message) : base(message)
+        {
+        }
+    }
+}
