011faab2ba4021ebe4c351626852cb7dc7b2d476
Andrea Nakstad <andrea.nakstad@yahoo.com>
2022-02-14 14:46:20 +0100

    test: created character and item test classes

---
+++ RPG-charactersTests/ItemTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xunit;
+
+namespace RPG_charactersTests
+{
+    public class ItemTests
+    {
+        [Fact]
+        public void Item_test1()
+        {
+            RPG_characters.PrimaryAttributes test = new RPG_characters.PrimaryAttributes();
+            RPG_characters.Mage testmage = new RPG_characters.Mage("some name", 2);
+            //PrimaryAttributes test = new PrimaryAttributes();
+            int expected = 0;
+            int real = test.Dexterity;
+            Assert.Equal(expected, real);
+        }
+    }
+}
