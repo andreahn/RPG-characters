1ad4359f8c4acd8e9756e51754247352560dd9df
Andrea Nakstad <andrea.nakstad@yahoo.com>
2022-02-14 16:16:46 +0100

    test: added unit tests for character

--- RPG-charactersTests/CharacterTests.cs
+++ RPG-charactersTests/CharacterTests.cs
@@ -1,19 +1,186 @@
 ﻿using System;
 using Xunit;
+using RPG_characters;
 
 namespace RPG_charactersTests
 {
     public class CharacterTests
     {
         [Fact]
-        public void Mage_Test1()
+        public void Character_OnCreation_IsLevelOne()
         {
-            RPG_characters.PrimaryAttributes test = new RPG_characters.PrimaryAttributes();
-            RPG_characters.Mage testmage = new RPG_characters.Mage("some name", 2);
-            //PrimaryAttributes test = new PrimaryAttributes();
-            int expected = 0;
-            int real = test.Dexterity;
-            Assert.Equal(expected, real);
+            // Arrange
+            Mage testCharacter = new Mage("");
+            int expected = 1;
+
+            // Act
+            int actual = testCharacter.Level;
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Character_OnLevelUp_IsLevelTwo()
+        {
+            // Arrange
+            Mage testCharacter = new Mage("");
+            int expected = 2;
+
+            // Act
+            testCharacter.LevelUp();
+            int actual = testCharacter.Level;
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Mage_OnCreation_HasDefaultAttributes()
+        {
+            // Arrange
+            Mage testCharacter = new Mage("");
+            int[] expected = new int[] {1, 1, 8};
+
+            // Act
+            int[] actual = new int[] {
+                testCharacter.Attributes.Strength,
+                testCharacter.Attributes.Dexterity,
+                testCharacter.Attributes.Intelligence
+            };
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Ranger_OnCreation_HasDefaultAttributes()
+        {
+            // Arrange
+            Ranger testCharacter = new Ranger("");
+            int[] expected = new int[] { 1, 7, 1 };
+
+            // Act
+            int[] actual = new int[] {
+                testCharacter.Attributes.Strength,
+                testCharacter.Attributes.Dexterity,
+                testCharacter.Attributes.Intelligence
+            };
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Rogue_OnCreation_HasDefaultAttributes()
+        {
+            // Arrange
+            Rogue testCharacter = new Rogue("");
+            int[] expected = new int[] { 2, 6, 1 };
+
+            // Act
+            int[] actual = new int[] {
+                testCharacter.Attributes.Strength,
+                testCharacter.Attributes.Dexterity,
+                testCharacter.Attributes.Intelligence
+            };
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Warrior_OnCreation_HasDefaultAttributes()
+        {
+            // Arrange
+            Warrior testCharacter = new Warrior("");
+            int[] expected = new int[] { 5, 2, 1 };
+
+            // Act
+            int[] actual = new int[] {
+                testCharacter.Attributes.Strength,
+                testCharacter.Attributes.Dexterity,
+                testCharacter.Attributes.Intelligence
+            };
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Mage_OnLevelUp_HasExpectedAttributes()
+        {
+            // Arrange
+            Mage testCharacter = new Mage("");
+            int[] expected = new int[] { 2, 2, 13 };
+
+            // Act
+            testCharacter.LevelUp();
+            int[] actual = new int[] {
+                testCharacter.Attributes.Strength,
+                testCharacter.Attributes.Dexterity,
+                testCharacter.Attributes.Intelligence
+            };
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Ranger_OnLevelUp_HasExpectedAttributes()
+        {
+            // Arrange
+            Ranger testCharacter = new Ranger("");
+            int[] expected = new int[] { 2, 12, 2 };
+
+            // Act
+            testCharacter.LevelUp();
+            int[] actual = new int[] {
+                testCharacter.Attributes.Strength,
+                testCharacter.Attributes.Dexterity,
+                testCharacter.Attributes.Intelligence
+            };
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Rogue_OnLevelUp_HasExpectedAttributes()
+        {
+            // Arrange
+            Rogue testCharacter = new Rogue("");
+            int[] expected = new int[] { 3, 10, 2 };
+
+            // Act
+            testCharacter.LevelUp();
+            int[] actual = new int[] {
+                testCharacter.Attributes.Strength,
+                testCharacter.Attributes.Dexterity,
+                testCharacter.Attributes.Intelligence
+            };
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Warrior_OnLevelUp_HasExpectedAttributes()
+        {
+            // Arrange
+            Warrior testCharacter = new Warrior("");
+            int[] expected = new int[] { 8, 4, 2 };
+
+            // Act
+            testCharacter.LevelUp();
+            int[] actual = new int[] {
+                testCharacter.Attributes.Strength,
+                testCharacter.Attributes.Dexterity,
+                testCharacter.Attributes.Intelligence
+            };
+
+            // Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
